58a1f49eb7d141554ddd35aa71d33b128ffc0cb0
Sahat Yalkabov <sakhat@gmail.com>
2015-02-05 02:01:40 -0800

    Gulp tasks for compiling, watching and livereload

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var watchify = require('watchify');
+var livereload = require('gulp-livereload');
+
+gulp.task('default', function() {
+  return scripts(false);
+});
+
+gulp.task('watch', function() {
+  return scripts(true);
+});
+
+function scripts(watch) {
+  var bundler, rebundle;
+  bundler = browserify('./react/main.js', {
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: watch
+  });
+
+  if (watch) {
+    bundler = watchify(bundler)
+  }
+
+  bundler.transform(reactify);
+
+  rebundle = function() {
+    return bundler.bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./public/javascripts'))
+      .pipe(livereload({ start: true }));
+
+  };
+
+  bundler.on('update', rebundle);
+  return rebundle();
+}
