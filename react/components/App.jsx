8da6f34f03020079ca20b1c95c065252f33e0e7d
Sahat Yalkabov <sakhat@gmail.com>
2015-02-05 02:02:36 -0800

    Initial App component

---
+++ react/components/App.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var MyView = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Sasa</h1>
+        But thesssn she said go away
+      </div>
+    );
+  }
+});
+
+module.exports = MyView;
