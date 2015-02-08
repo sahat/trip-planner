d3d0beee6cbda59843d39926d7a37be8a5e21be6
Sahat Yalkabov <sakhat@gmail.com>
2015-02-08 00:04:02 -0800

    Gulp new minify task and es6 option for reactify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
 var reactify = require('reactify');
 var watchify = require('watchify');
 var browserify = require('browserify');
@@ -12,7 +15,7 @@ gulp.task('browser-sync', function() {
   browserSync({
     server: {
       port: 3000,
-      baseDir: './public'
+      baseDir: './public/'
     }
   });
 });
@@ -21,22 +24,36 @@ gulp.task('sass', function() {
   gulp.src('./client/stylesheets/*.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest('./public/css/'))
     .pipe(reload({ stream: true }));
 });
 
 gulp.task('browserify', function() {
   var bundler = watchify(browserify('./client/main.js', watchify.args));
-  bundler.transform(reactify);
+  bundler.transform(function(file) {
+    return reactify(file, { es6: true });
+  });
   bundler.on('update', rebundle);
   function rebundle() {
     return plumber()
       .pipe(bundler.bundle())
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./public/js'))
+      .pipe(gulp.dest('./public/js/'))
       .pipe(reload({ stream: true }));
   }
   return rebundle();
+});
+
+gulp.task('minify', function() {
+  var css = gulp.src('./public/css/main.css')
+    .pipe(cssmin())
+    .pipe(gulp.dest('./public/css/'));
+
+  var js = gulp.src('./public/js/bundle.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'));
+
+  return merge(css, js);
 });
 
 gulp.task('watch', function() {
