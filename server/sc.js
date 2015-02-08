1e7299a2d3181c637ded844a35787ab20e2e7ac2
Sahat Yalkabov <sakhat@gmail.com>
2015-02-08 00:55:45 -0800

    Added lat and long to CA supercharger locations

--- server/sc.js
+++ server/sc.js
@@ -3,7 +3,10 @@ module.exports = [
   {
     location: 'Fremont',
     address: '45500 Fremont Blvd, Fremont, CA 94538 - Tesla Factory',
+    latitude: 37.49267,
+    longitude: -121.94409,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 120,
     description: ''
@@ -12,7 +15,10 @@ module.exports = [
   {
     location: 'Folsom',
     address: '13000 Folsom Blvd, Folsom, CA 95630 - Folsom Boulevard Exit 23 - Folsom Premium Outlets',
+    latitude: 38.64392,
+    longitude: -121.18621,
     state: 'CA',
+    country: 'United States',
     slots: 4,
     rate: 0,
     description: ''
@@ -21,7 +27,10 @@ module.exports = [
   {
     location: 'Gilroy',
     address: '681 Leavesley Rd, Gilroy, CA 95020 - 101 at Leavesley Road - Gilroy Premium Outlets - Near Sony',
+    latitude: 37.02615,
+    longitude: -121.56487,
     state: 'CA',
+    country: 'United States',
     slots: 10,
     rate: 120,
     description: ''
@@ -30,7 +39,10 @@ module.exports = [
   {
     location: 'Harris Ranch',
     address: 'I-5 Exit 334, Harris Ranch Inn and Restaurant 24505 W. Dorris Ave Coalinga, CA 93210',
+    latitude: 36.25316,
+    longitude: -120.23853,
     state: 'CA',
+    country: 'United States',
     slots: 7,
     rate: 120,
     description: ''
@@ -39,7 +51,10 @@ module.exports = [
   {
     location: 'Mt. Shasta',
     address: '111 Morgan Way, Mt Shasta, CA',
+    latitude: 41.30981,
+    longitude: -122.31766,
     state: 'CA',
+    country: 'United States',
     slots: 4,
     rate: 120,
     description: ''
@@ -48,7 +63,10 @@ module.exports = [
   {
     location: 'Roseville',
     address: '1151 Galleria Blvd, Roseville, CA 95678 - Westfield Galleria at Roseville',
+    latitude: 38.77344,
+    longitude: -121.26928,
     state: 'CA',
+    country: 'United States',
     slots: 7,
     rate: 135,
     description: ''
@@ -57,7 +75,10 @@ module.exports = [
   {
     location: 'Truckee',
     address: '11290 Donner Pass Road, Truckee, CA 96161',
+    latitude: 39.32720,
+    longitude: -120.20643,
     state: 'CA',
+    country: 'United States',
     slots: 6,
     rate: 135,
     description: ''
@@ -66,7 +87,10 @@ module.exports = [
   {
     location: 'Vacaville',
     address: '101 Nut Tree Rd, Vacaville, CA 95687 - Vacaville Premium Outlets',
+    latitude: 38.36720,
+    longitude: -121.95601,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 120,
     description: ''
@@ -75,7 +99,10 @@ module.exports = [
   {
     location: 'Atascadero',
     address: '6950 El Camino Real Atascadero, CA 93422 - Rabobank Parking Lot',
+    latitude: 35.48640,
+    longitude: -120.66558,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 120,
     description: ''
@@ -84,7 +111,10 @@ module.exports = [
   {
     location: 'Barstow',
     address: '1611 E Main St, Barstow, CA 92311 - I-15 Exit 178 - Country Inn and Suites',
+    latitude: 34.89120,
+    longitude: -116.99910,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 135,
     description: ''
@@ -93,7 +123,10 @@ module.exports = [
   {
     location: 'Buellton',
     address: '555 McMurray Rd, Buellton, CA 93427 - US0-101 Exit 140 - Marriott Hotel',
+    latitude: 34.61545,
+    longitude: -120.18856,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 120,
     description: ''
@@ -102,7 +135,10 @@ module.exports = [
   {
     location: 'Cabazon',
     address: '48400 Seminole Drive, Cabazon, CA 92230 - Desert Hills Premium Outlets',
+    latitude: 33.92967,
+    longitude: -116.81649,
     state: 'CA',
+    country: 'United States',
     slots: 10,
     rate: 135,
     description: ''
@@ -111,7 +147,10 @@ module.exports = [
   {
     location: 'Culver City',
     address: '6000 Sepulveda Blvd., Culver City, CA 90230 - Westfield Culver City',
+    latitude: 33.98465,
+    longitude: -118.39505,
     state: 'CA',
+    country: 'United States',
     slots: 12,
     rate: 135,
     description: ''
@@ -120,7 +159,10 @@ module.exports = [
   {
     location: 'El Centro',
     address: '3551 South Dogwood Road, El Centro, CA - Imperial Valley Mall',
+    latitude: 32.76200,
+    longitude: -115.53197,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 135,
     description: ''
@@ -129,7 +171,10 @@ module.exports = [
   {
     location: 'Indio',
     address: '42100 Jackson Street, Indio, CA 92203',
+    latitude: 33.74272,
+    longitude: -116.21316,
     state: 'CA',
+    country: 'United States',
     slots: 8,
     rate: 135,
     description: ''
@@ -138,7 +183,10 @@ module.exports = [
   {
     location: 'LA',
     address: '3203 Jack Northrop Ave, Hawthorne, CA 90250 - I-105 Exit 5 - SpaceX - Near Hawthorne Municipal Airport',
+    latitude: 33.92142,
+    longitude: 118.32982,
     state: 'CA',
+    country: 'United States',
     slots: 6,
     rate: 120,
     description: ''
@@ -147,7 +195,10 @@ module.exports = [
   {
     location: 'Needles',
     address: '2451 Needles Highway, Needles CA 92363',
+    latitude: 34.85083,
+    longitude: -114.62414,
     state: 'CA',
+    country: 'United States',
     slots: 4,
     rate: 135,
     description: ''
@@ -156,7 +207,10 @@ module.exports = [
   {
     location: 'Oxnard',
     address: '2751 Park View Court, Oxnard, CA 93036',
+    latitude: 34.24075,
+    longitude: -119.17699,
     state: 'CA',
+    country: 'United States',
     slots: 10,
     rate: 0,
     description: ''
@@ -165,7 +219,10 @@ module.exports = [
   {
     location: 'Rancho Cucamonga',
     address: '12642 Cultural Center Drive, Rancho Cucamonga, CA 91739 - Victoria Gardens Shopping Center',
+    latitude: 34.11348,
+    longitude: -117.53257,
     state: 'CA',
+    country: 'United States',
     slots: 12,
     rate: 135,
     description: ''
@@ -174,7 +231,10 @@ module.exports = [
   {
     location: 'San Juan Capistrano',
     address: '31971 Camino Capistrano, San Juan Capistrano, CA 92675 - Union Bank',
+    latitude: 33.49835,
+    longitude: -117.66287,
     state: 'CA',
+    country: 'United States',
     slots: 7,
     rate: 135,
     description: ''
@@ -183,7 +243,10 @@ module.exports = [
   {
     location: 'Tejon Ranch',
     address: '5602 Dennis McCarthy Dr, Lebec, CA 93243 - I-5 Exit 219B - Petro Shopping Center',
+    latitude: 34.98661,
+    longitude: -118.94630,
     state: 'CA',
+    country: 'United States',
     slots: 6,
     rate: 120,
     description: ''
