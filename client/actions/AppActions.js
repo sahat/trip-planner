0ad92bdbacf43e4cdc8c2af0a8454a77ae7a9cad
Sahat Yalkabov <sakhat@gmail.com>
2015-02-08 03:11:36 -0800

    New action and refactored AppActions

--- client/actions/AppActions.js
+++ client/actions/AppActions.js
@@ -1,12 +1,9 @@
 var Reflux = require('reflux');
 var request = require('superagent');
 
-var AppActions = Reflux.createActions([
-  'asdf'
-]);
-
-AppActions.loadSuperchargers = Reflux.createAction({
-  children: ['progressed', 'completed', 'failed']
+var AppActions = Reflux.createActions({
+  loadSuperchargers: { asyncResult: true },
+  getCurrentPosition: { asyncResult: true }
 });
 
 AppActions.loadSuperchargers.preEmit = function() {
