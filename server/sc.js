bff5220e51b0da7ff07dbf49ec03c50b474b9858
Sahat Yalkabov <sakhat@gmail.com>
2015-02-07 17:39:19 -0800

    Added list of superchargers for CA state

---
+++ server/sc.js
@@ -0,0 +1,192 @@
+module.exports = [
+
+  {
+    location: 'Fremont',
+    address: '45500 Fremont Blvd, Fremont, CA 94538 - Tesla Factory',
+    state: 'CA',
+    slots: 8,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Folsom',
+    address: '13000 Folsom Blvd, Folsom, CA 95630 - Folsom Boulevard Exit 23 - Folsom Premium Outlets',
+    state: 'CA',
+    slots: 4,
+    rate: 0,
+    description: ''
+  },
+
+  {
+    location: 'Gilroy',
+    address: '681 Leavesley Rd, Gilroy, CA 95020 - 101 at Leavesley Road - Gilroy Premium Outlets - Near Sony',
+    state: 'CA',
+    slots: 10,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Harris Ranch',
+    address: 'I-5 Exit 334, Harris Ranch Inn and Restaurant 24505 W. Dorris Ave Coalinga, CA 93210',
+    state: 'CA',
+    slots: 7,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Mt. Shasta',
+    address: '111 Morgan Way, Mt Shasta, CA',
+    state: 'CA',
+    slots: 4,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Roseville',
+    address: '1151 Galleria Blvd, Roseville, CA 95678 - Westfield Galleria at Roseville',
+    state: 'CA',
+    slots: 7,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Truckee',
+    address: '11290 Donner Pass Road, Truckee, CA 96161',
+    state: 'CA',
+    slots: 6,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Vacaville',
+    address: '101 Nut Tree Rd, Vacaville, CA 95687 - Vacaville Premium Outlets',
+    state: 'CA',
+    slots: 8,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Atascadero',
+    address: '6950 El Camino Real Atascadero, CA 93422 - Rabobank Parking Lot',
+    state: 'CA',
+    slots: 8,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Barstow',
+    address: '1611 E Main St, Barstow, CA 92311 - I-15 Exit 178 - Country Inn and Suites',
+    state: 'CA',
+    slots: 8,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Buellton',
+    address: '555 McMurray Rd, Buellton, CA 93427 - US0-101 Exit 140 - Marriott Hotel',
+    state: 'CA',
+    slots: 8,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Cabazon',
+    address: '48400 Seminole Drive, Cabazon, CA 92230 - Desert Hills Premium Outlets',
+    state: 'CA',
+    slots: 10,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Culver City',
+    address: '6000 Sepulveda Blvd., Culver City, CA 90230 - Westfield Culver City',
+    state: 'CA',
+    slots: 12,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'El Centro',
+    address: '3551 South Dogwood Road, El Centro, CA - Imperial Valley Mall',
+    state: 'CA',
+    slots: 8,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Indio',
+    address: '42100 Jackson Street, Indio, CA 92203',
+    state: 'CA',
+    slots: 8,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'LA',
+    address: '3203 Jack Northrop Ave, Hawthorne, CA 90250 - I-105 Exit 5 - SpaceX - Near Hawthorne Municipal Airport',
+    state: 'CA',
+    slots: 6,
+    rate: 120,
+    description: ''
+  },
+
+  {
+    location: 'Needles',
+    address: '2451 Needles Highway, Needles CA 92363',
+    state: 'CA',
+    slots: 4,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Oxnard',
+    address: '2751 Park View Court, Oxnard, CA 93036',
+    state: 'CA',
+    slots: 10,
+    rate: 0,
+    description: ''
+  },
+
+  {
+    location: 'Rancho Cucamonga',
+    address: '12642 Cultural Center Drive, Rancho Cucamonga, CA 91739 - Victoria Gardens Shopping Center',
+    state: 'CA',
+    slots: 12,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'San Juan Capistrano',
+    address: '31971 Camino Capistrano, San Juan Capistrano, CA 92675 - Union Bank',
+    state: 'CA',
+    slots: 7,
+    rate: 135,
+    description: ''
+  },
+
+  {
+    location: 'Tejon Ranch',
+    address: '5602 Dennis McCarthy Dr, Lebec, CA 93243 - I-5 Exit 219B - Petro Shopping Center',
+    state: 'CA',
+    slots: 6,
+    rate: 120,
+    description: ''
+  }
+
+];
