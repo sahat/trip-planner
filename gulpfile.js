1e2d259f1687430ab755fbbf4487ea95217e1b17
Sahat Yalkabov <sakhat@gmail.com>
2015-02-07 17:21:24 -0800

    Improved gulp tasks using browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,45 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 var reactify = require('reactify');
 var watchify = require('watchify');
-var livereload = require('gulp-livereload');
-var less = require('gulp-less');
+var browserify = require('browserify');
+var plumber = require('gulp-plumber');
+var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('less', function () {
-  gulp.src('public/stylesheets/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('public/stylesheets'));
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      port: 3000,
+      baseDir: './public'
+    }
+  });
 });
 
-gulp.task('default', function() {
-  return scripts(false);
+gulp.task('sass', function() {
+  gulp.src('./client/stylesheets/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('browserify', function() {
+  var bundler = watchify(browserify('./client/main.js', watchify.args));
+  bundler.transform(reactify);
+  bundler.on('update', rebundle);
+  function rebundle() {
+    return bundler.bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./public/js'))
+      .pipe(reload({ stream: true }));
+  }
+  return rebundle();
 });
 
 gulp.task('watch', function() {
-  return scripts(true);
+  gulp.watch('./client/stylesheets/**/*.scss', ['sass']);
 });
 
-function scripts(watch) {
-  var bundler, rebundle;
-  bundler = browserify('./src/main.js', {
-    debug: true,
-    cache: {},
-    packageCache: {},
-    fullPaths: watch
-  });
-
-  if (watch) {
-    bundler = watchify(bundler)
-  }
-
-  bundler.transform(function(file) {
-    return reactify(file, { es6: true });
-  });
-
-  rebundle = function() {
-    return bundler.bundle()
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./public/javascripts'))
-      .pipe(livereload({ start: true }));
-
-  };
-
-  bundler.on('update', rebundle);
-  return rebundle();
-}
+gulp.task('default', ['browser-sync', 'sass', 'browserify', 'watch']);
