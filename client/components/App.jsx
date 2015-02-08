5ac1eb0c41dcc3d8fa6e0190feddb8a9bb92a3c7
Sahat Yalkabov <sakhat@gmail.com>
2015-02-08 00:16:46 -0800

    Connected Reflux store to App component + ES6

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,33 @@
 var React = require('react');
+var Reflux = require('reflux');
+var AppStore = require('../stores/AppStore');
+var AppActions = require('../actions/AppActions');
 var Maps = require('./Maps.jsx');
 
 var App = React.createClass({
-  render: function() {
+
+  mixins: [Reflux.connect(AppStore, 'superchargers')],
+
+  componentDidMount() {
+    AppActions.loadSuperchargers();
+  },
+
+  componentDidUpdate() {
+  },
+
+  render() {
+    if (this.state.superchargers.length === 0) {
+      return null;
+    }
     return (
       <div id='container'>
-        <h1>sss examples...qweqsadsweqw.</h1>
+        <h1>sss exampless...qweqsadsweqw.</h1>
         But s  sssaid go awaywsdadsqw
         <Maps
+          superchargers={this.state.superchargers}
           latitude={37.4190421}
           longitude={-122.0254977}
-          zoom={16} />
+          zoom={10} />
       </div>
     );
   }
