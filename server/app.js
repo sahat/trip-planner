28a8becca97cf46d624016132b7a3d276d963599
Sahat Yalkabov <sakhat@gmail.com>
2015-02-18 17:06:59 -0800

    CORS Support for Node.js

--- server/app.js
+++ server/app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 //var mongoose = require('mongoose');
 var compression = require('compression');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var sc = require('./sc');
 
 var supercharger = require('./controllers/supercharger');
@@ -22,12 +21,11 @@ var supercharger = require('./controllers/supercharger');
 //  notes: String
 //});
 
-
-
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(compression());
+app.use(cors());
 app.use(favicon(__dirname + '/../public/favicon.png'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -35,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/supercharger', supercharger.getSuperchargerList);
+app.get('/superchargers', supercharger.getSuperchargerList);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port', app.get('port'));
